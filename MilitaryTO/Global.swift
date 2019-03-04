f82ec8bacd360e5ee8de8448aa1ab0dbc72c6bcd
Hyejun Jang <hyejun417@gmail.com>
2019-03-04 22:47:08 +0900

    파이어베이스 데이터 가져오기 테스트

---
+++ MilitaryTO/Global.swift
@@ -0,0 +1,11 @@
+//
+//  Global.swift
+//  MilitaryTO
+//
+//  Created by 장혜준 on 04/03/2019.
+//  Copyright © 2019 장혜준. All rights reserved.
+//
+
+import Foundation
+
+let myUserDefaults = UserDefaults.standard
