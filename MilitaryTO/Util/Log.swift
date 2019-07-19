736480df0c100b1cc540cabc97c5f169028f44cb
Hyejun Jang <jun@estmob.com>
2019-07-19 10:17:40 +0900

    Log 함수 os_log 로 개선

--- MilitaryTO/Util/Log.swift
+++ MilitaryTO/Util/Log.swift
@@ -7,17 +7,42 @@
 //
 
 import Foundation
+import os
 
-func DEBUG_LOG(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
+enum LogCategory: String {
+    // Default Category
+    case Debug
+    case Error
+}
+
+func DEBUG_LOG(_ msg: Any, category: LogCategory = .Debug, file: String = #file, function: String = #function, line: Int = #line) {
     #if DEBUG
-        let fileName = file.split(separator: "/").last ?? ""
-        let functionName = function.split(separator: "(").first ?? ""
-        print("👻 [\(fileName)] \(functionName)(\(line)) : \(msg)")
+        let fileName: String.SubSequence = file.split(separator: "/").last ?? "FileName is None"
+        let functionName: String.SubSequence = function.split(separator: "(").first ?? "FunctionName is None"
+        let logString: String = "[\(fileName)] \(functionName)(\(line)) : \(msg)"
+
+        if #available(iOS 10.0, *) {
+            os_log("%@",
+                log: OSLog(subsystem: Bundle.main.bundleIdentifier ?? "Unknown SubSystem",
+                    category: category.rawValue),
+                type: .info, logString)
+        } else {
+            NSLog("%@", logString)
+        }
     #endif
 }
 
-func ERROR_LOG(_ msg: Any, file: String = #file, function: String = #function, line: Int = #line) {
-    let fileName = file.split(separator: "/").last ?? ""
-    let functionName = function.split(separator: "(").first ?? ""
-    print("😡 [\(fileName)] \(functionName)(\(line)) : \(msg)")
+func ERROR_LOG(_ msg: Any, category: LogCategory, file: String = #file, function: String = #function, line: Int = #line) {
+    let fileName: String.SubSequence = file.split(separator: "/").last ?? "FileName is None"
+    let functionName: String.SubSequence = function.split(separator: "(").first ?? "FunctionName is None"
+    let logString: String = "[\(fileName)] \(functionName)(\(line)) : \(msg)"
+    
+    if #available(iOS 10.0, *) {
+        os_log("%@",
+               log: OSLog(subsystem: Bundle.main.bundleIdentifier ?? "Unknown SubSystem",
+                          category: category.rawValue),
+               type: .error, logString)
+    } else {
+        NSLog("%@", logString)
+    }
 }
